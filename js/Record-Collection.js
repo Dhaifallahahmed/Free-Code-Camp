1fd93f55118dfc85633a2cba34bb3122a9e852eb
DevDhaif <mmffrr2501@gmail.com>
2020-08-09 02:09:57 +0200

    record

--- js/Record-Collection.js
+++ js/Record-Collection.js
@@ -27,65 +27,27 @@ var collection = {
   
   // Only change code below this line
   function updateRecords(id, prop, value) {
-  if(prop!="tracks" && value!="" ){
-  collection[id][prop]="ABBA";
-  }
-  else if(prop=="tracks" && collection.id.tracks=="" )
-  {
-    arr=[];
-    collection[id][prop]="Take a Chance on Me";
-  }
-  else if(prop=="tracks" && value!="")
-  {
-    collection[id][prop].push(value);
-  }
-    return collection;
+  if(prop!=="tracks" && value!=="" ){
+  collection[id][prop]=value;
   }
   
-  updateRecords(5439, "artist", "ABBA");
-// Setup
-var collection = {
-    2548: {
-      album: "Slippery When Wet",
-      artist: "Bon Jovi",
-      tracks: [
-        "Let It Rock",
-        "You Give Love a Bad Name"
-      ]
-    },
-    2468: {
-      album: "1999",
-      artist: "Prince",
-      tracks: [
-        "1999",
-        "Little Red Corvette"
-      ]
-    },
-    1245: {
-      artist: "Robert Palmer",
-      tracks: [ ]
-    },
-    5439: {
-      album: "ABBA Gold"
-    }
-  };
-  
-  // Only change code below this line
-  function updateRecords(id, prop, value) {
-  if(prop!="tracks" && value!="" ){
-  collection[id][prop]="ABBA";
-  }
-  else if(prop=="tracks" && collection.id.tracks=="" )
+  else if(prop==="tracks" && value!=="")
   {
-    arr=[];
-    collection[id][prop]="Take a Chance on Me";
-  }
-  else if(prop=="tracks" && value!="")
-  {
-    collection[id][prop].push(value);
-  }
-    return collection;
-  }
-  
-  updateRecords(5439, "artist", "ABBA");
+   
+    collection[id][prop] = collection[id][prop] || [];
+      collection[id][prop].push(value);}
     
+  
+   else if(prop==="tracks" && value!=="")
+  {
+  collection[id][prop].push(value);
+  }
+  
+  else if(value===""){
+     delete collection[id][prop]
+  }
+    return collection;
+  }
+  
+  updateRecords(5439, "artist", "ABBA");
+  
